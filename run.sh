2080dee376062d9a5ce0dc949658f297ce9e9f20
Ioannis Giakoumidis <ioannis.giakoumidis@inviqa.com>
2018-02-01 11:09:19 +0000

    Check if git and ansible is installed

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# return 1 if global command line program installed, else 0
+# example
+# echo "node: $(program_is_installed git)"
+function is_program_installed {
+  # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
+
 cd ~
 if ! $(is_program_installed git)
 then
@@ -23,15 +35,3 @@ fi
 
 cd ~/ansible-provision
 ansible-playbook -i inventory base.yml $1
-
-# return 1 if global command line program installed, else 0
-# example
-# echo "node: $(program_is_installed git)"
-function is_program_installed {
-  # set to 1 initially
-  local return_=1
-  # set to 0 if not found
-  type $1 >/dev/null 2>&1 || { local return_=0; }
-  # return value
-  echo "$return_"
-}
