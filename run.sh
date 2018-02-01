65d36386988ef9a5b9852fbef6a198624cce7387
Ioannis Giakoumidis <ioannis.giakoumidis@inviqa.com>
2018-02-01 10:55:30 +0000

    Check if git and ansible is installed

--- run.sh
+++ run.sh
@@ -2,22 +2,34 @@
 
 # Clone the repository
 cd ~
-
-if [! -f ~/ansible-provision/firstrun_done]; then
-  sudo su -
-  apt-get update
-  apt-get install software-properties-common
-  apt-add-repository ppa:ansible/ansible
-  apt-get update
-  apt-get install ansible
-  apt-get install git
-
-  git clone git@github.com:ioannisGiak89/ansible-provision.git
-  cd ~/ansible-provision
-  touch firstrun_done
-else
-  cd ~/ansible-provision
-  git pull
+if [! is_program_installed git]; then
+  sudo apt-get update
+  sudo apt-get install git
 fi
 
+if [! is_program_installed ansible-playbook]; then
+  sudo apt-get update
+  sudo apt-get install software-properties-common
+  sudo apt-add-repository ppa:ansible/ansible
+  sudo apt-get update
+  sudo apt-get install ansible
+fi
+
+if [! -d ~/ansible-provision] then
+  git clone git@github.com:ioannisGiak89/ansible-provision.git
+fi
+
+cd ~/ansible-provision
 ansible-playbook -i inventory base.yml $1
+
+# return 1 if global command line program installed, else 0
+# example
+# echo "node: $(program_is_installed git)"
+function is_program_installed {
+  # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
