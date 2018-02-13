4e437b730651afb3748cae204987d08dcc3d1321
Ioannis Giakoumidis <ioannis.giakoumidis@inviqa.com>
2018-02-13 15:57:00 +0000

    Fix issue with Env

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -4,19 +4,20 @@ set -gx fish_greeting ""
 # Export
 set -gx GOPATH $HOME/.go
 set -gx PATH $GOPATH/bin $PATH
+
 # LESS with colors
 # from http://blog.0x1fff.com/2009/11/linux-tip-color-enabled-pager-less.html
-set -x LESS "-RSM~gIsw"
+set -gx LESS "-RSM~gIsw"
 
 # Colorful man pages
 # from http://pastie.org/pastes/206041/text
-setenv -x LESS_TERMCAP_mb (set_color -o red)
-setenv -x LESS_TERMCAP_md (set_color -o red)
-setenv -x LESS_TERMCAP_me (set_color normal)
-setenv -x LESS_TERMCAP_se (set_color normal)
-setenv -x LESS_TERMCAP_so (set_color -b blue -o yellow)
-setenv -x LESS_TERMCAP_ue (set_color normal)
-setenv -x LESS_TERMCAP_us (set_color -o green)
+set -gx LESS_TERMCAP_mb (set_color -o red)
+set -gx LESS_TERMCAP_md (set_color -o red)
+set -gx LESS_TERMCAP_me (set_color normal)
+set -gx LESS_TERMCAP_se (set_color normal)
+set -gx LESS_TERMCAP_so (set_color -b blue -o yellow)
+set -gx LESS_TERMCAP_ue (set_color normal)
+set -gx LESS_TERMCAP_us (set_color -o green)
 
 # Alias
-. (pwd)/ansible-provision/fish/files/alias.fish
+. (pwd)/ansible-provision/roles/fish/files/alias.fish
