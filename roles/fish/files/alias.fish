f0234ddbe8be5a2c99158629e5fe6d80057ff36d
Ioannis Giakoumidis <ioannis.giakoumidis@inviqa.com>
2018-02-13 16:07:42 +0000

    Fix issue with oh my fish

--- roles/fish/files/alias.fish
+++ roles/fish/files/alias.fish
@@ -1,25 +1,25 @@
 # Git
 
-alias gs='git status'
-alias gc='git checkout'
-alias gpo='git push -u origin'
-alias gp='git push'
-alias gm='git commit -m'
-alias gd='git diff'
-alias gb='git branch'
-alias ga='git add'
-alias gis='git stash'
-alias grh='git reset HEAD'
+alias gs="git status"
+alias gc="git checkout"
+alias gpo="git push -u origin"
+alias gp="git push"
+alias gm="git commit -m"
+alias gd="git diff"
+alias gb="git branch"
+alias ga="git add"
+alias gis="git stash"
+alias grh="git reset HEAD"
 
 # Mysql
-alias strmysql='sudo service mysql start'
-alias stpmysql='sudo service mysql stop'
-alias stsmysql='sudo service mysql status'
+alias strmysql="sudo service mysql start"
+alias stpmysql="sudo service mysql stop"
+alias stsmysql="sudo service mysql status"
 
 # Go
-alias gi='go install ./...'
-alias gwi='watch go install ./...'
-alias gwl='watch -n5 golint $(go list ./... | grep -v vendor | grep -v proto)'
+alias gi="go install ./..."
+alias gwi="watch go install ./..."
+alias gwl="watch -n5 golint (go list ./... | grep -v vendor | grep -v proto)"
 
 # Copy shh key
 alias cps="pbcopy < ~/.ssh/id_rsa.pub"
